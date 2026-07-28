4872a637615e78376f67e04e7ac852b2227a6b6d
Joe <a9650306@hotmail.com>
2026-07-28 08:18:42 +0800

    member_login&register

--- routes/front/member.php
+++ routes/front/member.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\front\MemberController as fm;  // frontmember 跟adminmember區隔開
 
 Route::group(["prefix" => "member"], function () {
-    Route::group(["middleware" => "membercheck"], function () {
+    Route::group(["middleware" => "memberlogin"], function () {
         Route::get("home", [fm::class, "home"]);
         Route::post("logout", [fm::class, "logout"]);
         Route::post("update", [fm::class, "update"]);
